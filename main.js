8f2a2efd5bfed290f3afabc920cb5596218fc4ba
muhammadrifqir <148336833+muhammadrifqir@users.noreply.github.com>
2024-03-27 14:59:50 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+/**
+ * Membuat component Main.
+ * Component Main menampung konten utama.
+ */
+function Main() {
+  return (
+    <main>
+      {/**
+       * Mengirim props ke component Hello.
+       * nama props: name
+       */}
+      <Hello name="Rizki" />
+      <Hello name="Rifqi" />
+      <Hello name="Hannah" />
+      <Hello name="Nathan" />
+      <Hello name="Martha" />
+    </main>
+  );
+}
+
+export default Main;
